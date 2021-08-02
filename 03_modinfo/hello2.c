a8b6db312ad6bceee739add25190ae4d308193cc
Joel Ding <joel.ding@avermedia.com>
2021-08-02 01:45:13 +0000

    Add modinfo 	new file:   03_modinfo/Makefile 	new file:   03_modinfo/hello2.c

---
+++ 03_modinfo/hello2.c
@@ -0,0 +1,41 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#ifdef __TEST__
+
+#include <linux/init.h>
+static int __init hello_2_init(void)
+{
+	printk("%s\n", __func__);
+	return 0;
+}
+
+module_init(hello_2_init);
+
+static void __exit hello_2_exit(void)
+{
+	printk("%s\n", __func__);
+}
+
+module_exit(hello_2_exit);
+
+#else
+
+int init_module(void)
+{
+	printk("%s\n", __func__);
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk("%s\n", __func__);
+}
+
+#endif
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("A003257");
+MODULE_DESCRIPTION("Test module");
+MODULE_SUPPORTED_DEVICE("");
+
